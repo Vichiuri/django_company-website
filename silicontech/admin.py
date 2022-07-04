5441bcb690a2e8357be76ad0892c8f4f6f61eb72
Vichiuri <chiuri.vic@gmail.com>
2022-07-05 00:13:57 +0300

    contact us page fix

--- silicontech/admin.py
+++ silicontech/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from silicontech.models import Contact
+
+
 # Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Contact, ProjectAdmin)
--- silicontech/views.py
+++ silicontech/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from silicontech.models import Contact
+from django.contrib import messages
 
 
 def project_index(request):
@@ -26,6 +28,22 @@ def team(request):
     return render(request, 'team.html')
 
 
-def contact(request):
+def contact(request): 
+    
+     if request.method == "POST":
+        contact = Contact()
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject =request.POST.get('subject')
+        message = request.POST.get('message')
 
-    return render(request, 'contact.html')
+        contact.name = name
+        contact.email = email
+        contact.subject =subject
+        contact.message = message
+        contact.save()
+        
+        messages.success(request, 'Your message has been sent. Thank you!')
+     return render(request, 'contact.html') 
+ 
+    
